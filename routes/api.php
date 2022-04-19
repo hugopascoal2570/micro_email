3fa7b7d93c7537571053b7d74e2384fa6857ffde
Hugo Pascoal <hugo_pascoal_@hotmail.com>
2022-04-18 21:21:09 -0300

    bug persistente ainda

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,13 @@
 <?php
 
+use App\Jobs\CompanyCreated;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/teste', function () {
+    CompanyCreated::dispatch('testestestes@email.com')->onQueue('queue_email');
+
+    return response()->json(['message' => 'success']);
+});
 
 Route::get('/', function () {
     return response()->json(['message' => 'success']);
